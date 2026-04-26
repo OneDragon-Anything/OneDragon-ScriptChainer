2bce32f035205528b4e5d28dfd862239b6a67177
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-04-26 09:30:01 +0800

    fix: 修复配置获取路径出错

--- src/one_dragon/utils/os_utils.py
+++ src/one_dragon/utils/os_utils.py
@@ -62,7 +62,7 @@ def get_work_dir() -> str:
     :return: 项目根目录
     """
     if run_in_exe():
-        return os.getcwd()
+        return os.path.dirname(sys.executable)
     dir_path: str = os.path.abspath(__file__)
     up_times = 4
     for _ in range(up_times):
